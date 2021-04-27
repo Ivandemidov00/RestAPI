767110ec337f425da13c4b0723a5b01cc51422a5
ivan.demidov00@mail.ru <ivan.demidov00@mail.ru>
2021-04-27 11:03:11 +0500

    model

---
+++ storeRestAPI/Model/order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeRestAPI.Model
+{
+    public class order
+    {
+        public Int16 id { get; set; }
+        public Int16 idProduct { get; set; }
+        public Int16 idUser { get; set; }
+        public Int16 amount { get; set; }
+        public Decimal price { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
